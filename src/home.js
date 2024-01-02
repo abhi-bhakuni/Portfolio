15d8f0e6c993e56aefe4cc6b0eaa77625235fc03
Abhishek Bhakuni <abhishek.bhakuni189@gmail.com>
2024-01-02 23:49:43 +0530

    bg img url update

--- src/home.js
+++ src/home.js
@@ -15,7 +15,7 @@ export default function Home() {
         <>
             <div className="nav">
                 <ul>
-                    <li><a href="">Home</a></li>
+                    <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#project">Projects</a></li>
